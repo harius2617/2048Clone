6989695c12971625c20486a5c55f966554a4e34f
harius2617 <danghai2617@gmail.com>
2021-12-10 11:18:49 +0700

    update

--- assets/Scripts/GameContainer.js
+++ assets/Scripts/GameContainer.js
@@ -13,26 +13,81 @@ cc.Class({
     
     properties: {
         item: cc.Prefab,
+        lbl2: cc.Sprite,
         
     },
     
     createItem(){
         const padX = 10;
         const padY = 10;
-
         for(let i = 1; i < 5; i++) {
             for(let j = 1; j < 5; j ++) {
                 let itemGame = cc.instantiate(this.item);
                 this.node.addChild(itemGame);
-                itemGame.setPosition(cc.v2(-100*i, 100*j))
+                itemGame.setPosition(cc.v2((itemGame.width+padX)*i-(itemGame.width)/2, (itemGame.height+padY)*j-(itemGame.height)/2))
             }
         }
 
+    },
+
+    onKeyRight: function(event){
+        switch(event.keyCode){
+            case cc.macro.KEY.right:
+                cc.log("right");
+                if(this.lbl2.node.x == 390) {
+                    this.lbl2.node.setPosition(cc.v2(this.lbl2.node.x, this.lbl2.node.y))
+                }else {
+                    this.lbl2.node.setPosition(cc.v2(this.lbl2.node.x+110,this.lbl2.node.y))
+                }
+                break;
+        }
+    },
+
+    onKeyLeft: function(event){
+        switch(event.keyCode){
+            case cc.macro.KEY.left:
+                cc.log("left");
+                if(this.lbl2.node.x == 390) {
+                    this.lbl2.node.setPosition(cc.v2(this.lbl2.node.x, this.lbl2.node.y))
+                }else {
+                    this.lbl2.node.setPosition(cc.v2(this.lbl2.node.x-110,this.lbl2.node.y))
+                }
+                break;
+        }
+    },
+
+    onKeyUp: function(event){
+        switch(event.keyCode){
+            case cc.macro.KEY.up:
+                cc.log("up");
+                if(this.lbl2.node.y == 390) {
+                    this.lbl2.node.setPosition(cc.v2(this.lbl2.node.x, this.lbl2.node.y))
+                }else {
+                    this.lbl2.node.setPosition(cc.v2(this.lbl2.node.x,this.lbl2.node.y+110))
+                }
+                break;
+        }
+    },
+    onKeyDown: function(event){
+        switch(event.keyCode){
+            case cc.macro.KEY.down:
+                cc.log("down");
+                if(this.lbl2.node.y == 390) {
+                    this.lbl2.node.setPosition(cc.v2(this.lbl2.node.x, this.lbl2.node.y))
+                }else {
+                    this.lbl2.node.setPosition(cc.v2(this.lbl2.node.x,this.lbl2.node.y-110))
+                }
+                break;
+        }
     },
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
         this.createItem();
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyRight, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyLeft, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyUp, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
         // cc.warn(this.item.node.position)
     },
 
