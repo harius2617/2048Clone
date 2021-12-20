2f171a449bd070de4892f15f799e4de7314f0cd8
harius2617 <danghai2617@gmail.com>
2021-12-20 08:21:16 +0700

    update

--- assets/Scripts/GameContainer.js
+++ assets/Scripts/GameContainer.js
@@ -1,63 +1,125 @@
+const PAD_X = 10;
+const PAD_Y = 10;
+const ROWS = 4;
+const COLS = 4;
+
 cc.Class({
+
     extends: cc.Component,
 
     properties: {
         block2: cc.Prefab,
         cell: cc.Prefab,
-        _padX: 10,
-        _padY: 10,
+        _lstBlock: [],
+        _lstPosition: [],
+        _lstEmptySlot: [],
         // _newItem: null,
-        arr: [],
-        dataPos: [],
         _posX: null,
         _posY: null,
+        canMove: {
+            default: true,
+            visible: false
+        }
     },
-    createItem() {
-        for (let i = 1; i < 5; i++) {
-            for (let j = 1; j < 5; j++) {
+    createBlockBg() {
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
                 let itemBg = cc.instantiate(this.cell);
                 this.node.addChild(itemBg);
-                itemBg.setPosition(cc.v2((itemBg.width + this._padX) * i - (itemBg.width) / 2, (itemBg.height + this._padY) * j - (itemBg.height) / 2))
+                const posX = (itemBg.width / 2) + (itemBg.width * i) + PAD_X * (i +1);
+                const posY = -((itemBg.height / 2) + (itemBg.height * j) + PAD_Y * (j + 1));
+                itemBg.setPosition(cc.v2(posX, posY))
+                this._lstPosition[j][i] = itemBg.position;
             }
         }
     },
 
-    randomAdd() { 
-        this._posX = Math.floor(Math.random() * 4) + 1;
-        this._posY = Math.floor(Math.random() * 4) + 1;
-    },
+    // randomAdd() { 
+    //     this._posX = Math.floor(Math.random() * 4) + 1;
+    //     this._posY = Math.floor(Math.random() * 4) + 1;
+    //     return this._posX, this._posY
+    // },
 
-    createArrBlock() {
+    createArray() {
+        this._lstBlock = [];
+        this._lstPosition = [];
+        this._lstEmptySlot = [];
         for(let i = 0; i < 4; i++) {
-            this.arr[i] = []
+            this._lstPosition[i] = [];
+            this._lstBlock[i] = [];
             for(let j = 0; j < 4; j++) {
-                this.arr[i][j] = 0
-                // this.dataPos[i][j] = 0
+                this._lstPosition[i][j] = 0;
+                this._lstBlock[i][j] = null;
+                this._lstEmptySlot.push({i,j});
             }
         }
+    },
+
+    randomPosition() {
+        if(this._lstEmptySlot.length <= 0) return;
+        const slot = Math.floor(Math.random() * this._lstEmptySlot.length);
+        const data = {
+            i: this._lstEmptySlot[slot].i,
+            j: this._lstEmptySlot[slot].j,
+            posX: this._lstPosition[this._lstEmptySlot[slot].i][this._lstEmptySlot[slot].j].x,
+            posY: this._lstPosition[this._lstEmptySlot[slot].i][this._lstEmptySlot[slot].j].y
+        }
+        this._lstEmptySlot.splice(slot, 1);
+        return data;
     },
 
     createNewBlock() {
-        let newItem = cc.instantiate(this.block2);
-        this.node.addChild(newItem)
-        this.randomAdd();
-        newItem.setPosition(cc.v2((newItem.width + this._padX) * this._posX - (newItem.width) / 2, (newItem.height + this._padY) * this._posY - (newItem.height) / 2))
-        this.arr[this._posX - 1][this._posY - 1] = newItem
-        cc.warn(this.arr)
+        let newBlock = cc.instantiate(this.block2);
+        this.node.addChild(newBlock)
+        // this.randomAdd();
+        let data = this.randomPosition();
+        newBlock.setPosition(cc.v2(data.posX, data.posY))
+        // this.arr[this._posX - 1][this._posY - 1] = newItem
+        // cc.warn(this.arr)
+        this._lstBlock[data.i][data.j] = newBlock;
+        newBlock.getComponent("blockControl").init();
+        newBlock.getComponent("blockControl").setCoordinates(data.i, data.j);
+
     },
 
     onKeyDown: function (event) {
         // cc.log(event.keyCode)
-        cc.tween(this.node)
-            .delay(1)
-            .call(()=>{
-                this.createNewBlock()
-            })
-            .start()
+        // cc.tween(this.node)
+        //     .delay(1)
+        //     .call(()=>{
+        //         this.createNewBlock()
+        //     })
+        //     .start()
+        // if(!this.canMove) return;
+        switch (event.keyCode) {
+            case cc.macro.KEY.right:
+                this.canMove = false
+                this.blockMoveRight();
+                break;
+            case cc.macro.KEY.left:
+                // this.blockMoveLeft();
+                break;
+            case cc.macro.KEY.up:
+                // this.blockMoveUp();
+                break;
+            case cc.macro.KEY.down:
+                // this.blockMoveDown();
+                break;
+        }
     },
 
     blockMoveRight() {
+        cc.warn('moveRight')
+        const moveCalculator = function(){
 
+        }
+
+        const lstBlock =this.getListBlockByTypeMove("RIGHT");
+        cc.warn(lstBlock);
+        for(let i = 0; i < lstBlock.length; i++){
+            const callBack = () => {};
+            moveCalculator(lstBlock[i], callBack)
+        }
     },
 
     newGame() {
@@ -68,6 +130,7 @@ cc.Class({
         this.arr = []
         this.createItem();
         this.createNewBlock();
+        cc.log(this._posX, this._posY)
         // this.createNewBlock();
         
     },
@@ -77,15 +140,16 @@ cc.Class({
     },
 
     onLoad() {
-        this.createArrBlock()
+        // this.createArrBlock()
         cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
     },
     
     start() {
+        this.createArray();
+        this.createBlockBg();
         this.createNewBlock();
         this.createNewBlock();
 
-        this.createItem();
         
     },
 });
--- assets/Scripts/blockControl.js
+++ assets/Scripts/blockControl.js
@@ -7,70 +7,98 @@ cc.Class({
     },
 
     onLoad() {
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyRight, this);
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyLeft, this);
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyUp, this);
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        // cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyRight, this);
+        // cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyLeft, this);
+        // cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyUp, this);
+        // cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
         // this.init()
     },
     
-
-    onKeyRight: function (event) {
-        switch (event.keyCode) {
-            case cc.macro.KEY.right:
-                // this.init()
-                if(this.node.x === 390){
-                    return;
-                }else{
-                    cc.tween(this.node)
-                        .to(0.5, { position: cc.v2(390, this.node.y) })
-                        .start()
-                    }
-                break;
-        }
+    properties: {
+        numLabel: cc.Label,
+        _value: 0,
+        _coordinateX: 0,
+        _coordinateX: 0,
     },
 
-    onKeyLeft: function (event) {
-        switch (event.keyCode) {
-            case cc.macro.KEY.left:
-                // cc.log("left");
-                if(this.node.x === 60){
-                    return
-                }else{
-                    cc.tween(this.node)
-                        .to(0.5, { position: cc.v2(60, this.node.y) })
-                        .start()
-                }
-                break
-        }
+    init(){
+        const num = Math.random() <= 0.9 ? 2 : 4;
+        this._value = num;
+        this.numLabel.string = num.toString();
     },
 
-    onKeyUp: function (event) {
-        switch (event.keyCode) {
-            case cc.macro.KEY.up:
-                // cc.log("up");
-                if(this.node.y == 390 && this.node.x === 60) {
-                    return;
-                }else {
-                    cc.tween(this.node)
-                        .to(0.5, { position: cc.v2(this.node.x, 390) })
-                        .start()
-                    }
-                break;
-        }
+    setCoordinates(i, j){
+        this._coordinateX = i;
+        this._coordinateY = j;
     },
-    onKeyDown: function (event) {
-        switch (event.keyCode) {
-            case cc.macro.KEY.down:
-                if(this.node.y == 60 && this.node.x === 60) {
 
-                    return;
-                }else {
-                    cc.tween(this.node)
-                        .to(0.5, { position: cc.v2(this.node.x, 60) })
-                        .start()
-                    }
-                break;
-        }
+    getCoordinates(){
+        return {i: this._coordinateX, j: this._coordinateY};
     },
+
+    setValue(val){
+        this._value = value;
+    },
+
+    getValue(){
+        return this._value;
+    }, 
+    // onKeyRight: function (event) {
+    //     switch (event.keyCode) {
+    //         case cc.macro.KEY.right:
+    //             // this.init()
+    //             if(this.node.x === 390){
+    //                 return;
+    //             }else{
+    //                 cc.tween(this.node)
+    //                     .to(0.5, { position: cc.v2(390, this.node.y) })
+    //                     .start()
+    //                 }
+    //             break;
+    //     }
+    // },
+
+    // onKeyLeft: function (event) {
+    //     switch (event.keyCode) {
+    //         case cc.macro.KEY.left:
+    //             // cc.log("left");
+    //             if(this.node.x === 60){
+    //                 return
+    //             }else{
+    //                 cc.tween(this.node)
+    //                     .to(0.5, { position: cc.v2(60, this.node.y) })
+    //                     .start()
+    //             }
+    //             break
+    //     }
+    // },
+
+    // onKeyUp: function (event) {
+    //     switch (event.keyCode) {
+    //         case cc.macro.KEY.up:
+    //             // cc.log("up");
+    //             if(this.node.y == 390 && this.node.x === 60) {
+    //                 return;
+    //             }else {
+    //                 cc.tween(this.node)
+    //                     .to(0.5, { position: cc.v2(this.node.x, 390) })
+    //                     .start()
+    //                 }
+    //             break;
+    //     }
+    // },
+    // onKeyDown: function (event) {
+    //     switch (event.keyCode) {
+    //         case cc.macro.KEY.down:
+    //             if(this.node.y == 60 && this.node.x === 60) {
+
+    //                 return;
+    //             }else {
+    //                 cc.tween(this.node)
+    //                     .to(0.5, { position: cc.v2(this.node.x, 60) })
+    //                     .start()
+    //                 }
+    //             break;
+    //     }
+    // },
 });
